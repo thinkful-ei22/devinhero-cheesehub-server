37163142e1338896f72e0abe573a014a9b55f562
dghero <devingreyhero@gmail.com>
2018-08-03 15:12:09 -0500

    Connect cheeses route to DB, GET cheeses

--- index.js
+++ index.js
@@ -31,6 +31,25 @@ app.use(
 //Mount routers
 app.use('/api/cheeses', cheeseRouter);
 
+
+//Custom 404 not found route
+app.use((req, res, next) =>{
+  const err = new Error('Not found');
+  err.status = 404;
+  next(err);
+});
+
+//Custom Error Handler
+app.use((err, req, res, next) =>{
+  if(err.status){
+    const errBody = Object.assign({}, err, {message: err.message});
+    res.status(err.status).json(errBody);
+  }else{
+    console.error(err);
+    res.status(500).json({message: 'Internal Server Error'});
+  }
+});
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
--- routes/cheeses.js
+++ routes/cheeses.js
@@ -10,26 +10,34 @@ const router = express.Router();
 
 router.get('/', (req, res, next)=>{
   
-  const cheeseList = [
-    'Bath Blue',
-    'Barkham Blue',
-    'Buxton Blue',
-    'Cheshire Blue',
-    'Devon Blue',
-    'Dorset Blue Vinney',
-    'Dovedale',
-    'Exmoor Blue',
-    'Harbourne Blue',
-    'Lanark Blue',
-    'Lymeswold',
-    'Oxford Blue',
-    'Shropshire Blue',
-    'Stichelton',
-    'Stilton',
-    'Blue Wensleydale',
-    'Yorkshire Blue'
-  ];
-  res.json(cheeseList);
+  Cheese.find().sort('name')
+    .then(results =>{
+      return res.json(results);
+    })
+    .catch(err =>{
+      next(err);
+    });
+
+  // const cheeseList = [
+  //   'Bath Blue',
+  //   'Barkham Blue',
+  //   'Buxton Blue',
+  //   'Cheshire Blue',
+  //   'Devon Blue',
+  //   'Dorset Blue Vinney',
+  //   'Dovedale',
+  //   'Exmoor Blue',
+  //   'Harbourne Blue',
+  //   'Lanark Blue',
+  //   'Lymeswold',
+  //   'Oxford Blue',
+  //   'Shropshire Blue',
+  //   'Stichelton',
+  //   'Stilton',
+  //   'Blue Wensleydale',
+  //   'Yorkshire Blue'
+  // ];
+  // res.json(cheeseList);
   
 });
 
